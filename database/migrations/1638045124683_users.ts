cdd7fc2b3e638e83f60ef501734c02f7bd221705
Dave-Costa <dorivaldo.programador@gmail.com>
2021-11-28 02:09:20 +0100

    feat: add migration user

---
+++ database/migrations/1638045124683_users.ts
@@ -0,0 +1,24 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class Users extends BaseSchema {
+  protected tableName = 'users'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.string('name')
+      table.string('last_name')
+      table.string('local')
+      table.string('email').unique()
+      table.string('password')
+      table.string('bi_number').unique()
+      table.string('photo')
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
