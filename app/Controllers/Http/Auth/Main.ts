c0cbcf9ec286535fb2b76763aba1cbdcd74be240
Dave-Costa <dorivaldo.programador@gmail.com>
2021-12-01 23:00:51 +0100

    chore: set middlewares for auth

---
+++ app/Controllers/Http/Auth/Main.ts
@@ -0,0 +1,24 @@
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { StoreValidator } from 'App/Validators'
+
+export default class AuthController {
+  public async store({ request, auth }: HttpContextContract) {
+    const { email, biNumber, password } = await request.validate(StoreValidator)
+    if (email) {
+      const token = await auth.attempt(email, password, {
+        expiresIn: '15 days',
+      })
+      return token
+    }
+    if (biNumber) {
+      const token = await auth.attempt(biNumber, password, {
+        expiresIn: '15 days',
+      })
+      return token
+    }
+  }
+
+  public async destroy({ auth }: HttpContextContract) {
+    await auth.logout()
+  }
+}
